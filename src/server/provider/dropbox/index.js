docsend/uppy

commit 3835149543ffddeb3ebdc4978b9fe6e4a0726712

Rewrite Companion providers to use streams to allow simultaneous upload/download without saving to disk (#3159)
* rewrite to async/await

* Only fetch size (HEAD) if needed #3034

* Update packages/@uppy/companion/src/server/controllers/url.js

Co-authored-by: Antoine du Hamel <duhamelantoine1995@gmail.com>

* Change HEAD to GET in getURLMeta

and abort request immediately upon response headers received
https://github.com/transloadit/uppy/issues/3034#issuecomment-908059234

* fix lint

* fix lint

* cut off length of file names

or else we get
"MetadataTooLarge: Your metadata headers exceed the maximum allowed metadata size" in tus / S3

* try to fix flaky test

* remove iife and cleanup code a bit

* fix lint by reordering code

* rename Uploader to MultipartUploader

* Rewrite Uploader to use fs-capacitor #3098

This allows for upload to start almost immediately without having to first download the file.
And it allows for uploading bigger files, because transloadit assembly will not timeout,
as it will get upload progress events all the time.
No longer need for illusive progress.
Also fix eslint warnings and simplify logic

Still TODO: TUS pause/resume has a bug:
https://github.com/tus/tus-js-client/issues/275

* add comment in dev Dashboard and pull out variable

* fix a bug where remote xhr upload would ignore progress events in the UI

* fix bug where s3 multipart cancel wasn't working

* fix also cancel for xhr

* Rewrite providers to use streams

This removes the need for disk space as data will be buffered in memory and backpressure will be respected
https://github.com/transloadit/uppy/issues/3098#issuecomment-907763809
All providers "download" methods will now return a { stream } which can be consumed by uploader.

Also:
- Remove capacitor (no longer needed)
- Change Provider/SearchProvider API to async (Breaking change for custom companion providers)

src/server/provider/dropbox/index.js
@@ -1,10 +1,12 @@
-const Provider = require('../Provider')
-
 const request = require('request')
 const purest = require('purest')({ request })
+const { promisify } = require('util')
+
+const Provider = require('../Provider')
 const logger = require('../../logger')
 const adapter = require('./adapter')
 const { ProviderApiError, ProviderAuthError } = require('../error')
+const { requestStream } = require('../../helpers/utils')
 
 // From https://www.dropbox.com/developers/reference/json-encoding:
 //
@@ -52,7 +54,7 @@ class DropBox extends Provider {
    * @param {object} options
    * @param {Function} done
    */
-  list (options, done) {
+  _list (options, done) {
     let userInfoDone = false
     let statsDone = false
     let userInfo
@@ -119,32 +121,26 @@ class DropBox extends Provider {
       .request(done)
   }
 
-  download ({ id, token }, onData) {
-    return this.client
-      .post('https://content.dropboxapi.com/2/files/download')
-      .options({
-        version: '2',
-        headers: {
-          'Dropbox-API-Arg': httpHeaderSafeJson({ path: `${id}` }),
-        },
-      })
-      .auth(token)
-      .request()
-      .on('response', (resp) => {
-        if (resp.statusCode !== 200) {
-          onData(this._error(null, resp))
-        } else {
-          resp.on('data', (chunk) => onData(null, chunk))
-        }
-      })
-      .on('end', () => onData(null, null))
-      .on('error', (err) => {
-        logger.error(err, 'provider.dropbox.download.error')
-        onData(err)
-      })
+  async download ({ id, token }) {
+    try {
+      const req = this.client
+        .post('https://content.dropboxapi.com/2/files/download')
+        .options({
+          version: '2',
+          headers: {
+            'Dropbox-API-Arg': httpHeaderSafeJson({ path: `${id}` }),
+          },
+        })
+        .auth(token)
+
+      return await requestStream(req, async (res) => this._error(null, res))
+    } catch (err) {
+      logger.error(err, 'provider.dropbox.download.error')
+      throw err
+    }
   }
 
-  thumbnail ({ id, token }, done) {
+  _thumbnail ({ id, token }, done) {
     return this.client
       .post('https://content.dropboxapi.com/2/files/get_thumbnail')
       .options({
@@ -168,7 +164,7 @@ class DropBox extends Provider {
       })
   }
 
-  size ({ id, token }, done) {
+  _size ({ id, token }, done) {
     return this.client
       .post('files/get_metadata')
       .options({ version: '2' })
@@ -180,11 +176,11 @@ class DropBox extends Provider {
           logger.error(err, 'provider.dropbox.size.error')
           return done(err)
         }
-        done(null, parseInt(body.size))
+        done(null, parseInt(body.size, 10))
       })
   }
 
-  logout ({ token }, done) {
+  _logout ({ token }, done) {
     return this.client
       .post('auth/token/revoke')
       .options({ version: '2' })
@@ -231,5 +227,12 @@ class DropBox extends Provider {
     return err
   }
 }
+
+DropBox.version = 2
+
+DropBox.prototype.list = promisify(DropBox.prototype._list)
+DropBox.prototype.thumbnail = promisify(DropBox.prototype._thumbnail)
+DropBox.prototype.size = promisify(DropBox.prototype._size)
+DropBox.prototype.logout = promisify(DropBox.prototype._logout)
 
 module.exports = DropBox

src/server/provider/box/index.js
@@ -1,10 +1,12 @@
-const Provider = require('../Provider')
-
 const request = require('request')
 const purest = require('purest')({ request })
+const { promisify } = require('util')
+
+const Provider = require('../Provider')
 const logger = require('../../logger')
 const adapter = require('./adapter')
 const { ProviderApiError, ProviderAuthError } = require('../error')
+const { requestStream } = require('../../helpers/utils')
 
 const BOX_FILES_FIELDS = 'id,modified_at,name,permissions,size,type'
 const BOX_THUMBNAIL_SIZE = 256
@@ -41,7 +43,7 @@ class Box extends Provider {
    * @param {object} options
    * @param {Function} done
    */
-  list ({ directory, token, query, companion }, done) {
+  _list ({ directory, token, query, companion }, done) {
     const rootFolderID = '0'
     const path = `folders/${directory || rootFolderID}/items`
 
@@ -66,26 +68,20 @@ class Box extends Provider {
       })
   }
 
-  download ({ id, token }, onData) {
-    return this.client
-      .get(`files/${id}/content`)
-      .auth(token)
-      .request()
-      .on('response', (resp) => {
-        if (resp.statusCode !== 200) {
-          onData(this._error(null, resp))
-        } else {
-          resp.on('data', (chunk) => onData(null, chunk))
-        }
-      })
-      .on('end', () => onData(null, null))
-      .on('error', (err) => {
-        logger.error(err, 'provider.box.download.error')
-        onData(err)
-      })
+  async download ({ id, token }) {
+    try {
+      const req = this.client
+        .get(`files/${id}/content`)
+        .auth(token)
+        .request()
+      return await requestStream(req, async (res) => this._error(null, res))
+    } catch (err) {
+      logger.error(err, 'provider.box.download.error')
+      throw err
+    }
   }
 
-  thumbnail ({ id, token }, done) {
+  _thumbnail ({ id, token }, done) {
     return this.client
       .get(`files/${id}/thumbnail.png`)
       .qs({ max_height: BOX_THUMBNAIL_SIZE, max_width: BOX_THUMBNAIL_SIZE })
@@ -120,7 +116,7 @@ class Box extends Provider {
       })
   }
 
-  size ({ id, token }, done) {
+  _size ({ id, token }, done) {
     return this.client
       .get(`files/${id}`)
       .auth(token)
@@ -130,11 +126,11 @@ class Box extends Provider {
           logger.error(err, 'provider.box.size.error')
           return done(err)
         }
-        done(null, parseInt(body.size))
+        done(null, parseInt(body.size, 10))
       })
   }
 
-  logout ({ companion, token }, done) {
+  _logout ({ companion, token }, done) {
     const { key, secret } = companion.options.providerOptions.box
 
     return this.client
@@ -189,5 +185,12 @@ class Box extends Provider {
     return err
   }
 }
+
+Box.version = 2
+
+Box.prototype.list = promisify(Box.prototype._list)
+Box.prototype.thumbnail = promisify(Box.prototype._thumbnail)
+Box.prototype.size = promisify(Box.prototype._size)
+Box.prototype.logout = promisify(Box.prototype._logout)
 
 module.exports = Box

src/server/provider/drive/index.js
@@ -1,12 +1,13 @@
 /* eslint-disable no-underscore-dangle */
-const { callbackify } = require('util')
 const request = require('request')
 const purest = require('purest')({ request })
+const { promisify } = require('util')
 
 const Provider = require('../Provider')
 const logger = require('../../logger')
 const adapter = require('./adapter')
 const { ProviderApiError, ProviderAuthError } = require('../error')
+const { requestStream } = require('../../helpers/utils')
 
 const DRIVE_FILE_FIELDS = 'kind,id,imageMediaMetadata,name,mimeType,ownedByMe,permissions(role,emailAddress),size,modifiedTime,iconLink,thumbnailLink,teamDriveId,videoMediaMetadata,shortcutDetails(targetId,targetMimeType)'
 const DRIVE_FILES_FIELDS = `kind,nextPageToken,incompleteSearch,files(${DRIVE_FILE_FIELDS})`
@@ -20,19 +21,15 @@ function sortByName (first, second) {
   return first.name.localeCompare(second.name)
 }
 
-function waitForFailedResponse (resp) {
-  return new Promise((resolve, reject) => {
+async function waitForFailedResponse (resp) {
+  const buf = await new Promise((resolve) => {
     let data = ''
     resp.on('data', (chunk) => {
       data += chunk
-    }).on('end', () => {
-      try {
-        resolve(JSON.parse(data.toString()))
-      } catch (error) {
-        reject(error)
-      }
-    })
+    }).on('end', () => resolve(data))
+    resp.resume()
   })
+  return JSON.parse(buf.toString())
 }
 
 function adaptData (listFilesResp, sharedDrivesResp, directory, query, showSharedWithMe) {
@@ -104,7 +101,7 @@ class Drive extends Provider {
     return 'google'
   }
 
-  async _list (options) {
+  async list (options) {
     const directory = options.directory || 'root'
     const query = options.query || {}
 
@@ -180,12 +177,7 @@ class Drive extends Provider {
     )
   }
 
-  list (options, done) {
-    // @ts-ignore
-    callbackify(this._list.bind(this))(options, done)
-  }
-
-  async stats ({ id, token }) {
+  async _stats ({ id, token }) {
     const getStats = async (statsOfId) => new Promise((resolve, reject) => {
       this.client
         .query()
@@ -217,79 +209,59 @@ class Drive extends Provider {
       .request()
   }
 
-  download ({ id: idIn, token }, onData) {
-    this.stats({ id: idIn, token })
-      .then(({ mimeType, id }) => {
-        let requestStream
-        if (adapter.isGsuiteFile(mimeType)) {
-          requestStream = this._exportGsuiteFile(id, token, adapter.getGsuiteExportType(mimeType))
-        } else {
-          requestStream = this.client
-            .query()
-            .get(`files/${encodeURIComponent(id)}`)
-            .qs({ alt: 'media', supportsAllDrives: true })
-            .auth(token)
-            .request()
-        }
+  async download ({ id: idIn, token }) {
+    try {
+      const { mimeType, id } = await this._stats({ id: idIn, token })
 
-        requestStream
-          .on('response', (resp) => {
-            if (resp.statusCode !== 200) {
-              waitForFailedResponse(resp)
-                .then((jsonResp) => {
-                  onData(this._error(null, { ...resp, body: jsonResp }))
-                })
-                .catch((err2) => onData(this._error(err2, resp)))
-            } else {
-              resp.on('data', (chunk) => onData(null, chunk))
-            }
-          })
-          .on('end', () => onData(null, null))
-          .on('error', (err2) => {
-            logger.error(err2, 'provider.drive.download.error')
-            onData(err2)
-          })
+      const req = adapter.isGsuiteFile(mimeType)
+        ? this._exportGsuiteFile(id, token, adapter.getGsuiteExportType(mimeType))
+        : this.client
+          .query()
+          .get(`files/${encodeURIComponent(id)}`)
+          .qs({ alt: 'media', supportsAllDrives: true })
+          .auth(token)
+          .request()
+
+      return await requestStream(req, async (res) => {
+        try {
+          const jsonResp = await waitForFailedResponse(res)
+          return this._error(null, { ...res, body: jsonResp })
+        } catch (err2) {
+          return this._error(err2, res)
+        }
       })
-      .catch((err) => {
-        logger.error(err, 'provider.drive.download.stats.error')
-        onData(err)
-      })
+    } catch (err) {
+      logger.error(err, 'provider.drive.download.error')
+      throw err
+    }
   }
 
   // eslint-disable-next-line class-methods-use-this
-  thumbnail (_, done) {
+  async thumbnail () {
     // not implementing this because a public thumbnail from googledrive will be used instead
-    const err = new Error('call to thumbnail is not implemented')
-    logger.error(err, 'provider.drive.thumbnail.error')
-    return done(err)
+    logger.error('call to thumbnail is not implemented', 'provider.drive.thumbnail.error')
+    throw new Error('call to thumbnail is not implemented')
   }
 
-  async _size ({ id, token }) {
+  async size ({ id, token }) {
     try {
-      const body = await this.stats({ id, token })
+      const { mimeType, size } = await this._stats({ id, token })
 
-      if (adapter.isGsuiteFile(body.mimeType)) {
-        // Not all GSuite file sizes can be predetermined
-        // also for files whose size can be predetermined,
-        // the request to get it can be sometimes expesnive, depending
-        // on the file size. So we default the size to the size export limit
-        const maxExportFileSize = 10 * 1024 * 1024 // 10 MB
-        return maxExportFileSize
+      if (adapter.isGsuiteFile(mimeType)) {
+        // GSuite file sizes cannot be predetermined (but are max 10MB)
+        // e.g. Transfer-Encoding: chunked
+        return undefined
       }
-      return parseInt(body.size, 10)
+
+      return parseInt(size, 10)
     } catch (err) {
       logger.error(err, 'provider.drive.size.error')
       throw err
     }
   }
 
-  size (options, done) {
-    // @ts-ignore
-    callbackify(this._size.bind(this))(options, done)
-  }
-
-  logout ({ token }, done) {
-    return this.client
+  _logout ({ token }, done) {
+    this.client
       .get('https://accounts.google.com/o/oauth2/revoke')
       .qs({ token })
       .request((err, resp) => {
@@ -311,5 +283,9 @@ class Drive extends Provider {
     return err
   }
 }
+
+Drive.version = 2
+
+Drive.prototype.logout = promisify(Drive.prototype._logout)
 
 module.exports = Drive

src/server/provider/facebook/index.js
@@ -1,11 +1,13 @@
-const Provider = require('../Provider')
-
 const request = require('request')
 const purest = require('purest')({ request })
+const { promisify } = require('util')
+
+const Provider = require('../Provider')
 const { getURLMeta } = require('../../helpers/request')
 const logger = require('../../logger')
 const adapter = require('./adapter')
 const { ProviderApiError, ProviderAuthError } = require('../error')
+const { requestStream } = require('../../helpers/utils')
 
 /**
  * Adapter for API https://developers.facebook.com/docs/graph-api/using-graph-api/
@@ -24,7 +26,7 @@ class Facebook extends Provider {
     return 'facebook'
   }
 
-  list ({ directory, token, query = { cursor: null } }, done) {
+  _list ({ directory, token, query = { cursor: null } }, done) {
     const qs = {
       fields: 'name,cover_photo,created_time,type',
     }
@@ -79,43 +81,41 @@ class Facebook extends Provider {
     return sortedImages[sortedImages.length - 1].source
   }
 
-  download ({ id, token }, onData) {
-    return this.client
-      .get(`https://graph.facebook.com/${id}`)
-      .qs({ fields: 'images' })
-      .auth(token)
-      .request((err, resp, body) => {
-        if (err || resp.statusCode !== 200) {
-          err = this._error(err, resp)
-          logger.error(err, 'provider.facebook.download.error')
-          onData(err)
-          return
-        }
-
-        request(this._getMediaUrl(body))
-          .on('response', (resp) => {
-            if (resp.statusCode !== 200) {
-              onData(this._error(null, resp))
-            } else {
-              resp.on('data', (chunk) => onData(null, chunk))
+  async download ({ id, token }) {
+    try {
+      const body1 = await new Promise((resolve, reject) => (
+        this.client
+          .get(`https://graph.facebook.com/${id}`)
+          .qs({ fields: 'images' })
+          .auth(token)
+          .request((err, resp, body) => {
+            if (err || resp.statusCode !== 200) {
+              err = this._error(err, resp)
+              logger.error(err, 'provider.facebook.download.error')
+              reject(err)
+              return
             }
+            resolve(body)
           })
-          .on('end', () => onData(null, null))
-          .on('error', (err) => {
-            logger.error(err, 'provider.facebook.download.url.error')
-            onData(err)
-          })
-      })
+      ))
+
+      const url = this._getMediaUrl(body1)
+      const req = request(url)
+      return await requestStream(req, async (res) => this._error(null, res))
+    } catch (err) {
+      logger.error(err, 'provider.facebook.download.url.error')
+      throw err
+    }
   }
 
-  thumbnail (_, done) {
+  // eslint-disable-next-line class-methods-use-this
+  async thumbnail () {
     // not implementing this because a public thumbnail from facebook will be used instead
-    const err = new Error('call to thumbnail is not implemented')
-    logger.error(err, 'provider.facebook.thumbnail.error')
-    return done(err)
+    logger.error('call to thumbnail is not implemented', 'provider.facebook.thumbnail.error')
+    throw new Error('call to thumbnail is not implemented')
   }
 
-  size ({ id, token }, done) {
+  _size ({ id, token }, done) {
     return this.client
       .get(`https://graph.facebook.com/${id}`)
       .qs({ fields: 'images' })
@@ -129,14 +129,14 @@ class Facebook extends Provider {
 
         getURLMeta(this._getMediaUrl(body))
           .then(({ size }) => done(null, size))
-          .catch((err) => {
-            logger.error(err, 'provider.facebook.size.error')
-            done()
+          .catch((err2) => {
+            logger.error(err2, 'provider.facebook.size.error')
+            done(err2)
           })
       })
   }
 
-  logout ({ token }, done) {
+  _logout ({ token }, done) {
     return this.client
       .delete('me/permissions')
       .auth(token)
@@ -186,5 +186,11 @@ class Facebook extends Provider {
     return err
   }
 }
+
+Facebook.version = 2
+
+Facebook.prototype.list = promisify(Facebook.prototype._list)
+Facebook.prototype.size = promisify(Facebook.prototype._size)
+Facebook.prototype.logout = promisify(Facebook.prototype._logout)
 
 module.exports = Facebook

src/server/provider/index.js
@@ -2,7 +2,7 @@
  * @module provider
  */
 // @ts-ignore
-const config = require('@purest/providers')
+const purestConfig = require('@purest/providers')
 const dropbox = require('./dropbox')
 const box = require('./box')
 const drive = require('./drive')
@@ -18,9 +18,10 @@ const { getCredentialsResolver } = require('./credentials')
 const Provider = require('./Provider')
 // eslint-disable-next-line
 const SearchProvider = require('./SearchProvider')
+const { wrapLegacyProvider } = require('./ProviderCompat')
 
 // leave here for now until Purest Providers gets updated with Zoom provider
-config.zoom = {
+purestConfig.zoom = {
   'https://zoom.us/': {
     __domain: {
       auth: {
@@ -45,6 +46,25 @@ config.zoom = {
 }
 
 /**
+ *
+ * @param {{server: object}} options
+ */
+const validOptions = (options) => {
+  return options.server.host && options.server.protocol
+}
+
+/**
+ *
+ * @param {string} name of the provider
+ * @param {{server: object, providerOptions: object}} options
+ * @returns {string} the authProvider for this provider
+ */
+const providerNameToAuthName = (name, options) => { // eslint-disable-line no-unused-vars
+  const providers = exports.getDefaultProviders()
+  return (providers[name] || {}).authProvider
+}
+
+/**
  * adds the desired provider module to the request object,
  * based on the providerName parameter specified
  *
@@ -60,8 +80,14 @@ module.exports.getProviderMiddleware = (providers, needsProviderCredentials) => 
    * @param {string} providerName
    */
   const middleware = (req, res, next, providerName) => {
-    if (providers[providerName] && validOptions(req.companion.options)) {
-      req.companion.provider = new providers[providerName]({ providerName, config })
+    let ProviderClass = providers[providerName]
+    if (ProviderClass && validOptions(req.companion.options)) {
+      // TODO remove this legacy provider compatibility when we release a new major
+      // @ts-ignore
+      if (ProviderClass.version !== 2) ProviderClass = wrapLegacyProvider(ProviderClass)
+
+      req.companion.provider = new ProviderClass({ providerName, config: purestConfig })
+
       if (needsProviderCredentials) {
         req.companion.getProviderCredentials = getCredentialsResolver(providerName, req.companion.options, req)
       }
@@ -100,14 +126,17 @@ module.exports.getSearchProviders = () => {
  */
 module.exports.addCustomProviders = (customProviders, providers, grantConfig) => {
   Object.keys(customProviders).forEach((providerName) => {
-    providers[providerName] = customProviders[providerName].module
-    const providerConfig = { ...customProviders[providerName].config }
-    // todo: consider setting these options from a universal point also used
-    // by official providers. It'll prevent these from getting left out if the
-    // requirement changes.
-    providerConfig.callback = `/${providerName}/callback`
-    providerConfig.transport = 'session'
-    grantConfig[providerName] = providerConfig
+    const customProvider = customProviders[providerName]
+
+    providers[providerName] = customProvider.module
+    grantConfig[providerName] = {
+      ...customProvider.config,
+      // todo: consider setting these options from a universal point also used
+      // by official providers. It'll prevent these from getting left out if the
+      // requirement changes.
+      callback: `/${providerName}/callback`,
+      transport: 'session',
+    }
   })
 }
 
@@ -163,23 +192,4 @@ module.exports.addProviderOptions = (companionOptions, grantConfig) => {
       logger.warn(`skipping one found unsupported provider "${providerName}".`, 'provider.options.skip')
     }
   })
-}
-
-/**
- *
- * @param {string} name of the provider
- * @param {{server: object, providerOptions: object}} options
- * @returns {string} the authProvider for this provider
- */
-const providerNameToAuthName = (name, options) => { // eslint-disable-line no-unused-vars
-  const providers = exports.getDefaultProviders()
-  return (providers[name] || {}).authProvider
-}
-
-/**
- *
- * @param {{server: object}} options
- */
-const validOptions = (options) => {
-  return options.server.host && options.server.protocol
 }

src/server/provider/instagram/graph/index.js
@@ -1,11 +1,13 @@
-const Provider = require('../../Provider')
-
 const request = require('request')
 const purest = require('purest')({ request })
+const { promisify } = require('util')
+
+const Provider = require('../../Provider')
 const { getURLMeta } = require('../../../helpers/request')
 const logger = require('../../../logger')
 const adapter = require('./adapter')
 const { ProviderApiError, ProviderAuthError } = require('../../error')
+const { requestStream } = require('../../../helpers/utils')
 
 /**
  * Adapter for API https://developers.facebook.com/docs/instagram-api/overview
@@ -31,7 +33,7 @@ class Instagram extends Provider {
     return 'instagram'
   }
 
-  list ({ directory, token, query = { cursor: null } }, done) {
+  _list ({ directory, token, query = { cursor: null } }, done) {
     const qs = {
       fields: 'id,media_type,thumbnail_url,media_url,timestamp,children{media_type,media_url,thumbnail_url,timestamp}',
     }
@@ -72,43 +74,39 @@ class Instagram extends Provider {
       })
   }
 
-  download ({ id, token }, onData) {
-    return this.client
-      .get(`https://graph.instagram.com/${id}`)
-      .qs({ fields: 'media_url' })
-      .auth(token)
-      .request((err, resp, body) => {
-        if (err || resp.statusCode !== 200) {
-          err = this._error(err, resp)
-          logger.error(err, 'provider.instagram.download.error')
-          onData(err)
-          return
-        }
-
-        request(body.media_url)
-          .on('response', (resp) => {
-            if (resp.statusCode !== 200) {
-              onData(this._error(null, resp))
-            } else {
-              resp.on('data', (chunk) => onData(null, chunk))
+  async download ({ id, token }) {
+    try {
+      const body1 = await new Promise((resolve, reject) => (
+        this.client
+          .get(`https://graph.instagram.com/${id}`)
+          .qs({ fields: 'media_url' })
+          .auth(token)
+          .request((err, resp, body) => {
+            if (err || resp.statusCode !== 200) {
+              err = this._error(err, resp)
+              logger.error(err, 'provider.instagram.download.error')
+              reject(err)
+              return
             }
+            resolve(body)
           })
-          .on('end', () => onData(null, null))
-          .on('error', (err) => {
-            logger.error(err, 'provider.instagram.download.url.error')
-            onData(err)
-          })
-      })
+      ))
+
+      const req = request(body1.media_url)
+      return await requestStream(req, async (res) => this._error(null, res))
+    } catch (err) {
+      logger.error(err, 'provider.instagram.download.url.error')
+      throw err
+    }
   }
 
-  thumbnail (_, done) {
+  async thumbnail () {
     // not implementing this because a public thumbnail from instagram will be used instead
-    const err = new Error('call to thumbnail is not implemented')
-    logger.error(err, 'provider.instagram.thumbnail.error')
-    return done(err)
+    logger.error('call to thumbnail is not implemented', 'provider.instagram.thumbnail.error')
+    throw new Error('call to thumbnail is not implemented')
   }
 
-  size ({ id, token }, done) {
+  _size ({ id, token }, done) {
     return this.client
       .get(`https://graph.instagram.com/${id}`)
       .qs({ fields: 'media_url' })
@@ -122,16 +120,16 @@ class Instagram extends Provider {
 
         getURLMeta(body.media_url)
           .then(({ size }) => done(null, size))
-          .catch((err) => {
-            logger.error(err, 'provider.instagram.size.error')
-            done()
+          .catch((err2) => {
+            logger.error(err2, 'provider.instagram.size.error')
+            done(err2)
           })
       })
   }
 
-  logout (_, done) {
+  async logout () {
     // access revoke is not supported by Instagram's API
-    done(null, { revoked: false, manual_revoke_url: 'https://www.instagram.com/accounts/manage_access/' })
+    return { revoked: false, manual_revoke_url: 'https://www.instagram.com/accounts/manage_access/' }
   }
 
   adaptData (res, username, directory, currentQuery) {
@@ -170,5 +168,10 @@ class Instagram extends Provider {
     return err
   }
 }
+
+Instagram.version = 2
+
+Instagram.prototype.list = promisify(Instagram.prototype._list)
+Instagram.prototype.size = promisify(Instagram.prototype._size)
 
 module.exports = Instagram

commit 0dc288a340ac13638cf31bb6e8b6d46ebbdb81b7

Fix bug introduced in #3159

src/server/provider/dropbox/index.js
@@ -132,6 +132,7 @@ class DropBox extends Provider {
           },
         })
         .auth(token)
+        .request()
 
       return await requestStream(req, async (res) => this._error(null, res))
     } catch (err) {